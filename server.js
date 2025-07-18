33a46c948d3e4a82c3fc7a2d3eb28672028987a1
Sindhuja M <sindhujaaa.00@gmail.com>
2025-07-19 01:19:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "portfolio_frontend")));
+
+// Handle all routes (for SPA)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'portfolio_frontend', 'index.html'));
+});
+
+
+app.get('/', (req, res) => {
+    res.send('✅ Contact backend is running!');
+});
+
+// Route
+app.post('/contact', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+        return res.status(400).json({ msg: 'Please fill all fields' });
+    }
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.MY_EMAIL,
+                pass: process.env.MY_PASSWORD,
+            },
+        });
+
+        await transporter.sendMail({
+            from: email,
+            to: process.env.MY_EMAIL,
+            subject: `Portfolio Message from ${name}`,
+            text: message,
+        });
+
+        res.status(200).json({ msg: 'Message sent successfully!' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Failed to send message. Try again later.' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
